ff7dba95b161138bbb6e4b0011826ac17586cdc5
rsafronov <electroma@gmail.com>
2017-04-04 12:00:08 -0400

    Added: CORS headers

--- src/rest/index.ts
+++ src/rest/index.ts
@@ -2,8 +2,12 @@ import * as express from 'express';
 import {AppConfig} from '../config/config';
 import {LocalRooms} from '../service/local/LocalRooms';
 import {registerBookitRest} from './server';
+import * as cors from 'cors';
 
-registerBookitRest(express(), new LocalRooms(AppConfig.roomLists))
+const app = express();
+app.use(cors());
+
+registerBookitRest(app, new LocalRooms(AppConfig.roomLists))
   .listen(AppConfig.port, () => {
     console.log('Ready');
   });
