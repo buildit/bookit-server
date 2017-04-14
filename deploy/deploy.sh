eaf37de8c2ed63fbb9f288fea5c38208b2f70f65
rsafronov <electroma@gmail.com>
2017-04-14 16:32:17 -0400

    Added: ssh deployment

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,6 +3,6 @@
 DIR=`dirname $0`
 
 chmod 600 $DIR/ec2/travis
-scp  -i $DIR/ec2/travis $DIR/dev/docker-compose.yml app@bookit.riglet.io:/home/app/
+scp -o StrictHostKeyChecking=no -i $DIR/ec2/travis $DIR/dev/docker-compose.yml app@bookit.riglet.io:/home/app/
 ssh -o StrictHostKeyChecking=no -i $DIR/ec2/travis app@bookit.riglet.io \
 'docker-compose down; docker-compose pull; docker-compose up -d'
