6b62ae7a994edea6335f684c362bd0689b40a70f
andrew <defpearlpilot@yahoo.com>
2017-06-27 18:35:33 -0400

    meetings visibility
      - check if env is null when determining domain
      - typescript error in getRoomListings

--- src/config/domain.ts
+++ src/config/domain.ts
@@ -2,8 +2,12 @@ import {roman, contoso, test, digital, defaultIdentity} from './identities';
 import {Domain} from '../model/EnvironmentConfig';
 
 
-export function getDomain(env: string): Domain {
-  switch (env) {
+export function getDomain(_env: string): Domain {
+  if (!_env) {
+    return defaultIdentity.domain;
+  }
+
+  switch (_env.toLowerCase()) {
     case 'roman': {
       return roman.domain;
     }
