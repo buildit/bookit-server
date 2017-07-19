640142378e6700a33a50a5492b566f720b3617c9
Mike Thomas <mathomas@users.noreply.github.com>
2017-07-19 15:17:32 -0600

    Include run-in-aws script that sets up environment variables derived from AWS' SSM Parameter Store and executes bookit-server. The idea is that this script would be specified as the CMD (or Command) in the ECS task for the server.  The script requires the AWS CLI, so that is installed in the container, as well.

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,14 @@
 FROM node:8.1.2-alpine
+
+# Install AWS CLI
+RUN \
+	mkdir -p /aws && \
+	apk -Uuv add groff less python py-pip && \
+	pip install awscli && \
+	apk --purge -v del py-pip && \
+	rm /var/cache/apk/*
+
+
 RUN mkdir -p /usr/src/app
 
 # copy the app
--- deploy/run-in-aws.sh
+++ deploy/run-in-aws.sh
@@ -1,4 +1,6 @@
+REGION=us-east-1
+
 USE_CLOUD=true \
-CLOUD_CONFIG=`aws ssm get-parameters --name CLOUD_CONFIG --with-decryption --output text | cut -f 4` \
-TEST_SECRET=`aws ssm get-parameters --name TEST_SECRET --with-decryption --output text | cut -f 4` \
+CLOUD_CONFIG=`aws ssm get-parameters --name CLOUD_CONFIG --region ${REGION} --with-decryption --output text | cut -f 4` \
+TEST_SECRET=`aws ssm get-parameters --name TEST_SECRET --region ${REGION} --with-decryption --output text | cut -f 4` \
 npm start
