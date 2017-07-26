1b9210988050937d1ee29390d6890cd64f61e101
Mike Thomas <mathomas@users.noreply.github.com>
2017-07-26 14:59:49 -0600

    Make build/deploy script look like bookit-web.

---
+++ scripts/build-and-deploy.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+DIR=`dirname $0`
+
+if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_EVENT_TYPE" == "push" ]]; then
+    export IMG_VERSION=`node -p -e "require('./package.json').version"`;
+    echo "Building $IMG_VERSION";
+    npm prune --production;
+    docker build -t builditdigital/bookit-server:$IMG_VERSION .;
+    docker login -u="$DOCKER_USERNAME" -p="$DOCKER_PASSWORD";
+    docker push builditdigital/bookit-server:$IMG_VERSION;
+    docker tag builditdigital/bookit-server:$IMG_VERSION builditdigital/bookit-server:latest;
+    docker push builditdigital/bookit-server:latest;
+
+    openssl aes-256-cbc -K $encrypted_2d5b9f764c04_key -iv $encrypted_2d5b9f764c04_iv -in ./deploy/ec2/travis.enc -out ./deploy/ec2/travis -d
+    chmod 600 $DIR/ec2/travis
+    scp -o StrictHostKeyChecking=no -i $DIR/ec2/travis $DIR/dev/docker-compose.yml app@bookit.riglet.io:/home/app/
+    ssh -o StrictHostKeyChecking=no -i $DIR/ec2/travis app@bookit.riglet.io \
+      'docker-compose down; docker-compose pull; docker-compose up -d'
+fi
+
