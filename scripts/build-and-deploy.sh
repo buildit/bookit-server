b005aa9e22d49e8554b0589258ad37dda07be19f
Mike Thomas <mathomas@users.noreply.github.com>
2017-07-28 15:44:59 -0600

    bug-47:  Don't even try to use the python scripts to get the params.  We have the AWS CLI.

--- scripts/build-and-deploy.sh
+++ scripts/build-and-deploy.sh
@@ -18,7 +18,7 @@ if [[ "$TRAVIS_BRANCH" == "master" ]] && [[ "$TRAVIS_EVENT_TYPE" == "push" ]]; t
     ssh -o StrictHostKeyChecking=no -i $DEPLOY_HOME/ec2/travis app@bookit.riglet.io \
       'docker-compose pull; \
        docker-compose down; \
-       CLOUD_CONFIG="`/usr/src/app/bin/fetch_aws_param.py CLOUD_CONFIG`" \
-       BUILDIT_SECRET="`/usr/src/app/bin/fetch_aws_param.py BUILDIT_SECRET`" docker-compose up -d'
+       CLOUD_CONFIG=`aws ssm get-parameters --name CLOUD_CONFIG --with-decryption --output text | cut -f 4` \
+       BUILDIT_SECRET=`aws ssm get-parameters --name BUILDIT_SECRET --with-decryption --output text | cut -f 4`'
 fi
 
