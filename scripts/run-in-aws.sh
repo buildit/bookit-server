36483e84a88a01e86ce96e3af4e5b0242cd38971
Mike Thomas <mathomas@users.noreply.github.com>
2017-08-03 11:38:23 -0600

    33-deploy-to-ecs: Damn, the ECS introspection data is only available in the ECS host, not the container.  Turns out my previous attempt, using EC2 instance data was almost correct, but I had missed a comma in the output.  Comma banished!

--- scripts/run-in-aws.sh
+++ scripts/run-in-aws.sh
@@ -2,7 +2,7 @@
 
 if [ -z "${AWS_REGION}" ]; then
   echo "AWS_REGION not passed in.  Obtaining AWS region from ECS Container Agent."
-  AWS_REGION=$(curl -s http://localhost:51678/v1/tasks | grep 'Arn.*arn\:aws\:ecs' | head -1 | cut -d : -f 5)
+  AWS_REGION=$(curl -s http://169.254.169.254/latest/dynamic/instance-identity/document  | grep '"region"' | cut -d : -f 2 | tr -d \" | tr -d \,)
 fi
 echo "AWS_REGION is ${AWS_REGION}"
 
