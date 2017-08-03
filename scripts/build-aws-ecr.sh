f998eee11f0f059261a4539d36eddcf018fb7cd8
Mike Thomas <mathomas@users.noreply.github.com>
2017-08-03 15:24:00 -0600

    33-deploy-to-ecs: Define region using AWS CLI standard var.  This should flow a little better.

--- scripts/build-aws-ecr.sh
+++ scripts/build-aws-ecr.sh
@@ -4,9 +4,9 @@ pip install --user awscli
 PATH=$PATH:/$HOME/.local/bin
 
 REPO_NAME=$DOCKER_REPO
-echo "The AWS region is $AWS_REGION"
+echo "The AWS region is $AWS_DEFAULT_REGION"
 
-EXISTING_REPO=$(aws ecr describe-repositories --region $AWS_REGION --repository-names $REPO_NAME --output text 2>/dev/null)
+EXISTING_REPO=$(aws ecr describe-repositories --repository-names $REPO_NAME --output text 2>/dev/null)
 REPO_EXISTS=$(echo $?)
 
 if [[ $REPO_EXISTS == 0 ]]; then
@@ -14,13 +14,13 @@ if [[ $REPO_EXISTS == 0 ]]; then
   REPO=$(echo $EXISTING_REPO | cut -f 6 -d ' ')
 else
   echo "Creating repo"
-  REPO=$(aws ecr create-repository --repository-name $REPO_NAME --region $AWS_REGION --output text | cut -f 6)
+  REPO=$(aws ecr create-repository --repository-name $REPO_NAME --output text | cut -f 6)
   echo "Created repo:  $REPO"
 fi
 
-#eval $(aws ecr get-login --region $AWS_REGION)
+#eval $(aws ecr get-login)
 #use below locally when above fails
-eval $(aws ecr get-login --region $AWS_REGION | sed 's/-e none//')
+eval $(aws ecr get-login | sed 's/-e none//')
 
 echo "Preparing to tag, repo:  ${REPO},  sha: ${COMMIT_SHA}"
 docker build -t "$REPO:$COMMIT_SHA" -t "$REPO:latest" .
