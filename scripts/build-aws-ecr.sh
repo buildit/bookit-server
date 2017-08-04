0a36d8730d4047c4da319e03c658be28a4c26ff5
Mike Thomas <mathomas@users.noreply.github.com>
2017-08-04 16:35:42 -0600

    33-deploy-to-ecs: Make more readable (hopefully), and a bit of cleanup.

--- scripts/build-aws-ecr.sh
+++ scripts/build-aws-ecr.sh
@@ -3,18 +3,18 @@
 pip install --user awscli
 PATH=$PATH:/$HOME/.local/bin
 
-REPO_NAME=$DOCKER_REPO
-echo "The AWS region is $AWS_DEFAULT_REGION"
+REPO_NAME=${ECS_REPO}
+echo "The AWS region is ${AWS_DEFAULT_REGION}"
 
-EXISTING_REPO=$(aws ecr describe-repositories --repository-names $REPO_NAME --output text 2>/dev/null)
+EXISTING_REPO=$(aws ecr describe-repositories --repository-names ${REPO_NAME} --output text 2>/dev/null)
 REPO_EXISTS=$(echo $?)
 
-if [[ $REPO_EXISTS == 0 ]]; then
+if [[ ${REPO_EXISTS} == 0 ]]; then
   echo "Repo already exists ... skipping creation"
-  REPO=$(echo $EXISTING_REPO | cut -f 6 -d ' ')
+  REPO=$(echo ${EXISTING_REPO} | cut -f 6 -d ' ')
 else
   echo "Creating repo"
-  REPO=$(aws ecr create-repository --repository-name $REPO_NAME --output text | cut -f 6)
+  REPO=$(aws ecr create-repository --repository-name ${REPO_NAME} --output text | cut -f 6)
   echo "Created repo:  $REPO"
 fi
 
@@ -23,6 +23,6 @@ fi
 eval $(aws ecr get-login | sed 's/-e none//')
 
 echo "Preparing to tag, repo:  ${REPO},  sha: ${COMMIT_SHA}"
-docker build -t "$REPO:$COMMIT_SHA" -t "$REPO:latest" .
-docker push "$REPO:$COMMIT_SHA"
-docker push "$REPO:latest"
+docker build -t "${REPO}:${COMMIT_SHA}" -t "${REPO}:latest" .
+docker push "${REPO}:${COMMIT_SHA}"
+docker push "${REPO}:latest"
