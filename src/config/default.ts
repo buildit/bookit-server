5caeaa1538fe21c90dc9c8b0cc6ef55ded918520
Mike Thomas <mathomas@users.noreply.github.com>
2017-08-16 17:06:37 -0600

    53-aws-namespace-params:  Extract environment setup into function for testing.

--- src/config/default.ts
+++ src/config/default.ts
@@ -14,25 +14,25 @@ const checkFlag = (flag: string): boolean => {
   return flag.toLowerCase() === 'true';
 };
 
-/*
-Start creating the environment here
- */
-const environment: EnvironmentConfig = {};
 
-environment.domain = getDomain(AppEnv.CLOUD_CONFIG);
+function buildEnvironment() {
+  const environment: EnvironmentConfig = {};
+  environment.domain = getDomain(AppEnv.CLOUD_CONFIG);
 
-logger.info('Using azure?', AppEnv.USE_AZURE);
-if (AppEnv.USE_AZURE && AppEnv.USE_AZURE === 'true') {
-  logger.info('About to assign identity');
-  assignGraphIdentity(environment, AppEnv.CLOUD_CONFIG);
+  logger.info('Using azure?', AppEnv.USE_AZURE);
+  if (AppEnv.USE_AZURE && AppEnv.USE_AZURE === 'true') {
+    logger.info('About to assign identity');
+    assignGraphIdentity(environment, AppEnv.CLOUD_CONFIG);
+  }
+
+  environment.jwtTokenSecret = AppEnv.JWT_TOKEN_SECRET || 'testing secret';
+
+  environment.useMeetingCache = !checkFlag(AppEnv.MEETING_CACHE_DISABLED);
+  environment.useGroupCache = !checkFlag(AppEnv.GROUP_CACHE_DISABLED);
+
+  logger.info('Meeting Cache enabled:', environment.useMeetingCache);
+  logger.info('Group Cache enabled:', environment.useGroupCache);
+  return environment;
 }
 
-environment.jwtTokenSecret = AppEnv.JWT_TOKEN_SECRET || 'testing secret';
-
-environment.useMeetingCache = !checkFlag(AppEnv.MEETING_CACHE_DISABLED);
-environment.useGroupCache = !checkFlag(AppEnv.GROUP_CACHE_DISABLED);
-
-logger.info('Meeting Cache enabled:', environment.useMeetingCache);
-logger.info('Group Cache enabled:', environment.useGroupCache);
-
-module.exports = environment;
+module.exports = buildEnvironment();
