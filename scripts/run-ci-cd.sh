92a5f375d101bc64748152890aeb0336bc9834dd
Greg Pearman <greg.pearman@wipro.com>
2017-09-07 10:46:12 -0600

    - install awscli inside script instead of travis - Check that int deploy is successful before running integration tests

--- scripts/run-ci-cd.sh
+++ scripts/run-ci-cd.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-./scripts/ecs-deploy.sh -c ${INT_ECS_CLUSTER} -n ${INT_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA}
-./scripts/run-integration-tests.sh
-TEST_EXIT=$?
-if [ $TEST_EXIT -eq 0 ]; then ./scripts/ecs-deploy.sh -c ${STG_ECS_CLUSTER} -n ${STG_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA}; fi
+./scripts/ecs-deploy.sh -c ${INT_ECS_CLUSTER} -n ${INT_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA} \
+  && ./scripts/run-integration-tests.sh \
+  && ./scripts/ecs-deploy.sh -c ${STG_ECS_CLUSTER} -n ${STG_ECS_SERVICE} -i 006393696278.dkr.ecr.${AWS_DEFAULT_REGION}.amazonaws.com/${ECS_REPO}:${COMMIT_SHA}
